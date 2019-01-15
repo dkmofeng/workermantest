5f99aa53c74c5b07ceb0010eb2699ae409fd5f65
277 <277@qq.com>
2019-01-15 23:54:31 +0800

    's'

---
+++ web/index.php
@@ -0,0 +1,29 @@
+
+<script>
+    var websocket=new WebSocket("ws://129.204.72.68:2347");
+    websocket.onopen=function(e){
+        console.log(e);
+    }
+    websocket.onmessage=function(e){
+        console.log(e);
+    }
+    websocket.close=function(e){
+        console.log(e);
+    }
+    function ping(){
+        var jsondata={'type':'pong'};
+        websocket.send(jsondata);
+        setTimeout('ping()',10);
+    }
+    function sendmessage() {
+       var txt= document.getElementById('text').value;
+       if(txt!=''){
+           var jsondata={'type':'sayall','message':txt};
+           websocket.send(jsondata);
+       }
+    }
+</script>
+
+<input id="text">
+
+<button onclick="sendmessage()">发送消息</button>
